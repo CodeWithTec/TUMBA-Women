0a873ac518000c7824cd201f08749f3e27b6cf0f
CodeWithTec <codewithtec@example.com>
2025-08-16 15:56:05 +0000

    012

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,15 @@
 
 
+
+
+
+
+
+
+
+
+
+
 // pop up for all the pages 
 
 //  pop-up
@@ -57,7 +67,164 @@ closeBtn2.addEventListener("click", () => {
 });
 
 window.addEventListener("click", (event) => {
-    if (event.target === popup) {
+    if (event.target === popup2) {
         popup2.style.display = "none";
+    }
+});
+
+
+// third pop-up
+const popup3 = document.getElementById("popup3");
+const readMoreBtn3 = document.getElementById("readMoreBtn3");
+const closeBtn3 = document.getElementById("closeBtn3");
+
+readMoreBtn3.addEventListener("click", () => {
+    popup3.style.display = "block";
+});
+
+closeBtn3.addEventListener("click", () => {
+    popup3.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup3) {
+        popup3.style.display = "none";
+    }
+});
+
+
+
+// fouth pop-up
+const popup4 = document.getElementById("popup4");
+const readMoreBtn4 = document.getElementById("readMoreBtn4");
+const closeBtn4 = document.getElementById("closeBtn4");
+
+readMoreBtn4.addEventListener("click", () => {
+    popup4.style.display = "block";
+});
+
+closeBtn4.addEventListener("click", () => {
+    popup4.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup4) {
+        popup4.style.display = "none";
+    }
+});
+
+
+
+// fifth pop-up
+const popup5 = document.getElementById("popup5");
+const readMoreBtn5 = document.getElementById("readMoreBtn5");
+const closeBtn5 = document.getElementById("closeBtn5");
+
+readMoreBtn5.addEventListener("click", () => {
+    popup5.style.display = "block";
+});
+
+closeBtn5.addEventListener("click", () => {
+    popup5.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup5) {
+        popup5.style.display = "none";
+    }
+});
+
+// sixth pop-up
+const popup6 = document.getElementById("popup6");
+const readMoreBtn6 = document.getElementById("readMoreBtn6");
+const closeBtn6 = document.getElementById("closeBtn6");
+
+readMoreBtn6.addEventListener("click", () => {
+    popup6.style.display = "block";
+});
+
+closeBtn6.addEventListener("click", () => {
+    popup6.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup6) {
+        popup6.style.display = "none";
+    }
+});
+
+// seventh pop-up
+const popu75 = document.getElementById("popup7");
+const readMoreBtn7 = document.getElementById("readMoreBtn7");
+const closeBtn7 = document.getElementById("closeBtn7");
+
+readMoreBtn7.addEventListener("click", () => {
+    popup7.style.display = "block";
+});
+
+closeBtn7.addEventListener("click", () => {
+    popup7.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup7) {
+        popup7.style.display = "none";
+    }
+});
+
+// Eight pop-up
+const popup8 = document.getElementById("popup8");
+const readMoreBtn8 = document.getElementById("readMoreBtn8");
+const closeBtn8 = document.getElementById("closeBtn8");
+
+readMoreBtn8.addEventListener("click", () => {
+    popup8.style.display = "block";
+});
+
+closeBtn8.addEventListener("click", () => {
+    popup8.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup8) {
+        popup8.style.display = "none";
+    }
+});
+
+// Nine pop-up
+const popup9 = document.getElementById("popup9");
+const readMoreBtn9 = document.getElementById("readMoreBtn9");
+const closeBtn9 = document.getElementById("closeBtn9");
+
+readMoreBtn9.addEventListener("click", () => {
+    popup9.style.display = "block";
+});
+
+closeBtn9.addEventListener("click", () => {
+    popup9.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup9) {
+        popup9.style.display = "none";
+    }
+});
+
+// tenth pop-up
+const popup10 = document.getElementById("popup10");
+const readMoreBtn10 = document.getElementById("readMoreBtn10");
+const closeBtn10 = document.getElementById("closeBtn10");
+
+readMoreBtn10.addEventListener("click", () => {
+    popup10.style.display = "block";
+});
+
+closeBtn10.addEventListener("click", () => {
+    popup10.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === popup10) {
+        popup10.style.display = "none";
     }
 });
